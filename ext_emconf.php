c23a9b3fb885bbef11217194d8cae855885c196c
vnc-stel <sajan.telrejah@vancado.com>
2024-07-03 16:04:48 +0200

    [FEATURE] First Commit
    Tasks:
    * Add emconf for extension
    
    Related: #WPKK-52

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF['vnc_interactive_image'] = [
+    'title' => 'Vancado Interactive Image',
+    'description' => 'An extension to create interactive images with icons and markers.',
+    'category' => 'plugin',
+    'author' => 'Sajan Telrejah',
+    'author_email' => 'Sajan.Telrejah@Vancado.de',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];

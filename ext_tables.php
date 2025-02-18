466023c730dfbd964fb0e92cbbb7cfecfd90d924
Johannes Böttcher <johannes.boettcher@vancado.de>
2025-02-18 15:57:12 +0100

    [WIP] Add stylesheets for backend
    Resolves: #SWGR-109

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,12 @@
 defined('TYPO3') or die();
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 call_user_func(function () {
+    $typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
+
     ExtensionManagementUtility::addStaticFile(
         'vnc_interactive_image',
         'Configuration/TypoScript',
@@ -25,4 +29,12 @@ call_user_func(function () {
     ExtensionManagementUtility::allowTableOnStandardPages(
         'tx_vncinteractiveimage_domain_model_mark'
     );
+
+    if ($typo3Version->getMajorVersion() <= 11) {
+        $GLOBALS['TBE_STYLES']['skins']['vnc_interactive_image'] = [];
+        $GLOBALS['TBE_STYLES']['skins']['vnc_interactive_image']['name'] = 'BE Styles for InteractiveImage';
+        $GLOBALS['TBE_STYLES']['skins']['vnc_interactive_image']['stylesheetDirectories'] = [
+            'all' => 'EXT:vnc_interactive_image/Resources/Public/Css/Backend'
+        ];
+    }
 });
